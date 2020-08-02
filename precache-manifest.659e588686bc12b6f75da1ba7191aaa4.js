6819a7dd51ae782598df624f4e884739a2152742
Agustin <agustin.gomez@devlights.com>
2020-08-02 20:26:51 -0300

    Pushing buil folder

---
+++ precache-manifest.659e588686bc12b6f75da1ba7191aaa4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39f6f4043ee36f959c95cb74e2aaa2cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ed7196cd4ca0c2e9221",
+    "url": "/static/css/main.da05569d.chunk.css"
+  },
+  {
+    "revision": "57305e8ba82cd437a75a",
+    "url": "/static/js/2.bfb8ecf4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.bfb8ecf4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ed7196cd4ca0c2e9221",
+    "url": "/static/js/main.480c6522.chunk.js"
+  },
+  {
+    "revision": "4b9f09c12f2e24a55a1c",
+    "url": "/static/js/runtime-main.8a33c874.js"
+  }
+]);
